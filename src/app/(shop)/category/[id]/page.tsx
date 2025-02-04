489e4d6c47cc4008c15020f570271f1d7d632aa7
facchin21 <ferminfacchinq@gmail.com>
2025-02-04 12:03:13 -0300

    agrego pantalla de productos por slug, con detalles y carrusel de imagenes para mobile y desktop

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,26 +1,39 @@
-import { Title } from "@/components";
-import { notFound } from "next/navigation";
+import { ProductGrid, Title } from "@/components";
+import { Category } from "@/interfaces";
+import { initialData } from "@/seed/seed";
+// import { notFound } from "next/navigation";
 
 interface Props {
   params: {
-    id: string;
+    id: Category;
   }
 }
+const products = initialData.products;
 
 
 export default function CategoryPage({ params }: Props) {
   const { id } = params;
+  const productsFiltered = products.filter(product => product.gender === id);
 
-  if( id === 'kids'){
-    notFound();
+  const labels: Record<Category, string> = {
+    'men': 'para hombres',
+    'women': 'para mujeres',
+    'kid': 'para niños',
+    'unisex' : 'para todos'
   }
-  
+
+  // if( id === 'kids'){
+  //   notFound();
+  // }
+
   return (
     <>
-      <Title 
-        title='Tienda'
-        subtitle="Todos los productos"
-        className=""/>
+      <Title
+        title={`Articulos de ${labels[id]}`}
+        subtitle='Todos los productos'
+        className="" />
+
+      <ProductGrid products={productsFiltered} />
     </>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,11 @@
 
 
 // Archivo de Barril
+export { ProductMobileSlideshow } from './product/slideshow/ProductMobileSlideshow'
+export { QuantitySelector } from './product/quantity-selector/QuantitySelector';
 export { ProductGridItem } from './products/product-grid/ProductGridItem';
+export { ProductSlideshow } from './product/slideshow/ProductSlideshow';
+export { SizeSelector } from './product/size-selector/SizeSelector'
 export { ProductGrid } from './products/product-grid/ProductGrid';
 export { PageNotFound } from './ui/not-found/PageNotFound';
 export { ItemMenu } from './ui/top-menu/ItemMenu';
